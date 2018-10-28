8444586a36791e082a541c062085c149251e74b2
Joshua Trimble <jtrimble6@yahoo.com>
2018-10-28 16:55:49 -0500

    going live

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import PortfolioContainer from "./components/PortfolioContainer";
 import './App.css';
 
-const App = () =>  <PortfolioContainer />
+// const App = () =>  <PortfolioContainer />
 
-// const App = () => {
-//   <Router>
-//     <div>
-//       <NavTabs />
-//       <Route exact path="/" component={Home} />
-//       <Route exact path="/about" component={About} />
-//       <Route exact path="/projects" component={Projects} />
-//       <Route exact path="/contact" component={Contact} />
-//     </div>
-//   </Router>
-// }
+class App extends Component {
+
+  render () {
+      return(
+        <Router>
+          <div>
+            <Route path="*" render={() => <PortfolioContainer />}  />
+          </div>
+        </Router>
+      )
+  }
+  
+}
   
 export default App;
